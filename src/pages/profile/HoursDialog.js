a18e52c8e78f87185448a97ff0865d1ff1a92abf
Ashton Morris <ashtonmorris@Ashtons-MBP-2.home>
2019-03-21 16:18:05 -0400

    profile business logic

--- src/pages/profile/HoursDialog.js
+++ src/pages/profile/HoursDialog.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 
 import {
   Dialog,
@@ -9,172 +9,92 @@ import {
   PrimaryButton,
   DefaultButton
 } from "office-ui-fabric-react/lib/Button";
-import { Text } from "office-ui-fabric-react/lib/Text";
-import {
-  Dropdown,
-  DropdownMenuItemType
-} from "office-ui-fabric-react/lib/Dropdown";
 
-export default ({ hidden, closeDialog }) => (
-  <Dialog
-    hidden={hidden}
-    onDismiss={() => closeDialog("hoursDialog")}
-    minWidth="95%"
-    dialogContentProps={{
-      type: DialogType.largeHeader,
-      title: "Store Hours",
-      subText: "Lorem ipsum dolor sit amet."
-    }}
-    modalProps={{
-      isBlocking: false,
-      containerClassName: "ms-dialogMainOverride"
-    }}
-  >
-    <div className="hours-field">
-      <Dropdown
-        placeholder="Select an Option"
-        ariaLabel="Basic dropdown example"
-        options={[
-          {
-            key: "Header",
-            itemType: DropdownMenuItemType.Header
-          },
-          { key: "A", text: "Mon - Fri" },
-          { key: "B", text: "Sat - Sun" },
-          { key: "C", text: "Monday - Sunday" },
-          {
-            key: "divider_2",
-            text: "-",
-            itemType: DropdownMenuItemType.Divider
-          },
-          {
-            key: "Header2",
-            itemType: DropdownMenuItemType.Header
-          },
-          { key: "F", text: "Mon" },
-          { key: "G", text: "Tue" },
-          { key: "H", text: "Wed" },
-          { key: "I", text: "Thu" },
-          { key: "J", text: "Fri" },
-          { key: "J", text: "Sat" },
-          { key: "J", text: "Sun" }
-        ]}
-        selectedKey={"C"}
-      />
-      <Text className="hours-field__text" variant="xLarge">
-        From
-      </Text>
-      <Dropdown
-        placeholder="Select an Option"
-        ariaLabel="Basic dropdown example"
-        options={[
-          {
-            key: "Header2",
-            itemType: DropdownMenuItemType.Header
-          },
-          { key: "F", text: "5:00am" },
-          { key: "F", text: "5:30am" },
-          { key: "G", text: "6:00am" },
-          { key: "H", text: "6:30am" },
-          { key: "I", text: "7:00am" },
-          { key: "J", text: "7:30am" },
-          { key: "J", text: "8:00am" },
-          { key: "J", text: "8:30am" },
-          { key: "L", text: "9:00am" },
-          { key: "J", text: "9:30am" },
-          { key: "J", text: "10:00am" },
-          { key: "J", text: "10:30am" },
-          { key: "J", text: "11:00am" },
-          { key: "J", text: "11:30am" },
-          { key: "J", text: "12:00pm" },
-          { key: "J", text: "12:30pm" },
-          { key: "J", text: "1:00pm" },
-          { key: "J", text: "1:30pm" },
-          { key: "J", text: "2:00pm" },
-          { key: "J", text: "2:30pm" },
-          { key: "J", text: "3:00pm" },
-          { key: "J", text: "3:30pm" },
-          { key: "J", text: "4:00pm" },
-          { key: "J", text: "4:30pm" },
-          { key: "J", text: "5:00pm" },
-          { key: "J", text: "5:30pm" },
-          { key: "J", text: "6:00pm" },
-          { key: "J", text: "6:30pm" },
-          { key: "J", text: "7:00pm" },
-          { key: "J", text: "7:30pm" },
-          { key: "J", text: "8:00pm" },
-          { key: "J", text: "8:30pm" },
-          { key: "J", text: "9:00pm" },
-          { key: "J", text: "9:30pm" },
-          { key: "J", text: "10:00pm" },
-          { key: "J", text: "10:30pm" },
-          { key: "J", text: "11:00pm" },
-          { key: "J", text: "11:30pm" },
-          { key: "J", text: "12:00am" },
-          { key: "J", text: "12:30am" }
-        ]}
-        selectedKey={"L"}
-      />
-      <Text className="hours-field__text" variant="xLarge">
-        To
-      </Text>
-      <Dropdown
-        placeholder="Select an Option"
-        ariaLabel="Basic dropdown example"
-        options={[
-          {
-            key: "Header2",
-            itemType: DropdownMenuItemType.Header
-          },
-          { key: "F", text: "5:00am" },
-          { key: "F", text: "5:30am" },
-          { key: "G", text: "6:00am" },
-          { key: "H", text: "6:30am" },
-          { key: "I", text: "7:00am" },
-          { key: "J", text: "7:30am" },
-          { key: "J", text: "8:00am" },
-          { key: "J", text: "8:30am" },
-          { key: "J", text: "9:00am" },
-          { key: "J", text: "9:30am" },
-          { key: "J", text: "10:00am" },
-          { key: "J", text: "10:30am" },
-          { key: "J", text: "11:00am" },
-          { key: "J", text: "11:30am" },
-          { key: "J", text: "12:00pm" },
-          { key: "J", text: "12:30pm" },
-          { key: "J", text: "1:00pm" },
-          { key: "J", text: "1:30pm" },
-          { key: "J", text: "2:00pm" },
-          { key: "J", text: "2:30pm" },
-          { key: "J", text: "3:00pm" },
-          { key: "J", text: "3:30pm" },
-          { key: "J", text: "4:00pm" },
-          { key: "J", text: "4:30pm" },
-          { key: "L", text: "5:00pm" },
-          { key: "J", text: "5:30pm" },
-          { key: "J", text: "6:00pm" },
-          { key: "J", text: "6:30pm" },
-          { key: "J", text: "7:00pm" },
-          { key: "J", text: "7:30pm" },
-          { key: "J", text: "8:00pm" },
-          { key: "J", text: "8:30pm" },
-          { key: "J", text: "9:00pm" },
-          { key: "J", text: "9:30pm" },
-          { key: "J", text: "10:00pm" },
-          { key: "J", text: "10:30pm" },
-          { key: "J", text: "11:00pm" },
-          { key: "J", text: "11:30pm" },
-          { key: "J", text: "12:00am" },
-          { key: "J", text: "12:30am" }
-        ]}
-        selectedKey={"L"}
-      />
-    </div>
+import HoursField from "./HoursField";
 
-    <DialogFooter>
-      <PrimaryButton onClick={() => {}} text="Add Field" />
-      <PrimaryButton onClick={() => closeDialog("hoursDialog")} text="Save" />
-      <DefaultButton onClick={() => closeDialog("hoursDialog")} text="Cancel" />
-    </DialogFooter>
-  </Dialog>
-);
+import ProfileContext from "./profileContext";
+
+export default class HoursDialog extends Component {
+  state = {
+    fields: [],
+    hours: []
+  };
+
+  renderFields = store => {
+    const fields = [];
+    if (store.hours.length) {
+      store.hours.forEach((timeBlock, index) => {
+        const processedTimeBlock = timeBlock.split(/[\s-]+/);
+        fields.push(
+          <HoursField
+            key={index}
+            index={index}
+            setHours={this.setHours}
+            startTime={processedTimeBlock[0]}
+            endTime={
+              processedTimeBlock[0] === "CLOSED"
+                ? "CLOSED"
+                : processedTimeBlock[processedTimeBlock.length - 1]
+            }
+          />
+        );
+      });
+    } else {
+      for (let i = 0; i < 7; i++) {
+        fields.push(<HoursField key={i} index={i} setHours={this.setHours} />);
+      }
+    }
+
+    return fields;
+  };
+
+  setHours = (index, value) => {
+    const { hours } = this.state;
+
+    const hoursCopy = hours.slice();
+    hoursCopy[index] = value;
+
+    this.setState({
+      hours: hoursCopy
+    });
+  };
+
+  _onSave = () => {
+    const { closeDialog, onSave } = this.props;
+    onSave(this.state.hours);
+    closeDialog("hoursDialog");
+  };
+
+  render() {
+    const { hidden, closeDialog } = this.props;
+    return (
+      <ProfileContext.Consumer>
+        {({ store }) => (
+          <Dialog
+            hidden={hidden}
+            onDismiss={() => closeDialog("hoursDialog")}
+            minWidth="95%"
+            dialogContentProps={{
+              type: DialogType.largeHeader,
+              title: "Store Hours",
+              subText: "Lorem ipsum dolor sit amet."
+            }}
+            modalProps={{
+              isBlocking: false,
+              containerClassName: "ms-dialogMainOverride"
+            }}
+          >
+            {this.renderFields(store)}
+            <DialogFooter>
+              <PrimaryButton onClick={this._onSave} text="Save" />
+              <DefaultButton
+                onClick={() => closeDialog("hoursDialog")}
+                text="Cancel"
+              />
+            </DialogFooter>
+          </Dialog>
+        )}
+      </ProfileContext.Consumer>
+    );
+  }
+}
--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,21 +1,31 @@
 import React, { Component } from "react";
 import "./profile.scss";
 
-import {
-  Persona,
-  PersonaInitialsColor,
-  PersonaSize
-} from "office-ui-fabric-react/lib/Persona";
+import { Persona, PersonaSize } from "office-ui-fabric-react/lib/Persona";
 import { PrimaryButton } from "office-ui-fabric-react/lib/Button";
 import { Text } from "office-ui-fabric-react/lib/Text";
 
 import InformationDialog from "./InformationDialog";
 import HoursDialog from "./HoursDialog";
 
-const personaWithInitials = {
-  text: "Garden State Dispensary",
-  secondaryText: "950 US-1, Woodbridge, NJ 07095",
-  tertiaryText: "(973) 220 - 1995"
+import ProfileContext from "./profileContext";
+
+const store = {
+  id: "ea8055d1-1a4d-41a6-a4b7-2df74bfcb172",
+  name: "Greenleaf Compassion Center",
+  address: "395 Bloomfield Ave Montclair, NJ 07042",
+  phone: "(973)-337-5670",
+  image:
+    "https://static1.squarespace.com/static/56ba0d5659827eedd59d1ea1/t/596e5870be6594279e89fde0/1500403846718/Dispensary.png?format=2500w",
+  hours: [
+    "CLOSED",
+    "10:00am – 5:00pm",
+    "10:00am – 8:00pm",
+    "10:00am – 8:00pm",
+    "10:00am – 5:00pm",
+    "10:00am – 5:00pm",
+    "10:00am – 5:00pm"
+  ]
 };
 
 class Profile extends Component {
@@ -32,73 +42,112 @@ class Profile extends Component {
     this.setState({ [dialog]: true });
   };
 
-  _onChoiceChanged() {
-    console.log("Choice option change");
-  }
+  _onSaveInformation = changedValues => {
+    const valuesToChange = {};
+
+    for (let key in changedValues) {
+      if (changedValues[key].length) {
+        valuesToChange[key] = changedValues[key];
+      }
+    }
+
+    const changedValueKeys = Object.keys(valuesToChange);
+
+    changedValueKeys.map(key => (store[key] = changedValues[key]));
+  };
+
+  _renderDayWithIndex = index => {
+    switch (index) {
+      case 0:
+        return "Sun. ";
+      case 1:
+        return "Mon. ";
+      case 2:
+        return "Tues. ";
+      case 3:
+        return "Wed. ";
+      case 4:
+        return "Thurs. ";
+      case 5:
+        return "Fri. ";
+      default:
+        return "Sat. ";
+    }
+  };
+
+  _renderTimes = hours =>
+    hours.map((timeBlock, index) => (
+      <div className="hours-card__hours" key={index}>
+        <Text variant="large">{this._renderDayWithIndex(index)}</Text>
+        <Text variant="large">{timeBlock}</Text>
+      </div>
+    ));
+
+  _onSaveHours = changedValues => {
+    const mutatedHours = store.hours.slice();
+    changedValues.forEach((timeBlock, index) => {
+      if (timeBlock === "CLOSED - CLOSED") {
+        timeBlock = "CLOSED";
+      }
+      mutatedHours[index] = timeBlock;
+    });
+    if (mutatedHours.length < 7) {
+      for (let i = 0; i < 7; i++) {
+        if (!mutatedHours[i]) {
+          mutatedHours[i] = "CLOSED";
+        }
+      }
+    }
+    store.hours = mutatedHours;
+  };
 
   render() {
     const { infoDialog, hoursDialog } = this.state;
 
     return (
-      <div className="profile">
-        <div className="profile__details">
-          <Persona
-            {...personaWithInitials}
-            initialsColor={PersonaInitialsColor.teal}
-            coinSize={150}
-            onClick={() => this._showDialog("infoDialog")}
-            size={PersonaSize.size72}
-            imageAlt="GS"
-            imageUrl={
-              "https://static1.squarespace.com/static/56ba0d5659827eedd59d1ea1/t/596e5870be6594279e89fde0/1500403846718/Dispensary.png?format=2500w"
-            }
-            className="profile__info"
-            imageShouldFadeIn
-          />
-          <InformationDialog
-            hidden={infoDialog}
-            closeDialog={this._closeDialog}
-          />
-        </div>
-        <div className="profile__other">
-          <div className="hours-card">
-            <Text variant="xxLarge" className="hours-card__title">Open Hours</Text>
-
-            <div className="hours-card__hours">
-              <Text variant="xLarge">Mon: </Text>
-              <Text variant="xLarge">9am - 5pm</Text>
-            </div>
-
-            <div className="hours-card__hours">
-              <Text variant="xLarge">Tues: </Text>
-              <Text variant="xLarge">9am - 5pm</Text>
-            </div>
-
-            <div className="hours-card__hours">
-              <Text variant="xLarge">Wed: </Text>
-              <Text variant="xLarge">9am - 5pm</Text>
-            </div>
-
-            <div className="hours-card__hours">
-              <Text variant="xLarge">Thu: </Text>
-              <Text variant="xLarge">9am - 5pm</Text>
-            </div>
-
-            <div className="hours-card__hours">
-              <Text variant="xLarge">Fri: </Text>
-              <Text variant="xLarge">9am - 5pm</Text>
-            </div>
-
-            <PrimaryButton
-              className="hours-card__btn"
-              onClick={() => this._showDialog("hoursDialog")}
-              text="Update Hours"
+      <ProfileContext.Provider value={{ store }}>
+        <div className="profile">
+          <div className="profile__details">
+            <Persona
+              text={store.name}
+              secondaryText={store.address}
+              tertiaryText={store.phone}
+              coinSize={150}
+              onClick={() => this._showDialog("infoDialog")}
+              size={PersonaSize.size72}
+              imageUrl={store.image}
+              className="profile__info"
+              imageShouldFadeIn
             />
+            <InformationDialog
+              hidden={infoDialog}
+              closeDialog={this._closeDialog}
+              store={store}
+              onSave={this._onSaveInformation}
+            />
+          </div>
 
-            <HoursDialog hidden={hoursDialog} closeDialog={this._closeDialog} />
+          <div className="profile__other">
+            <div className="hours-card">
+              <Text variant="xxLarge" className="hours-card__title">
+                Open Hours
+              </Text>
+              {this._renderTimes(store.hours)}
+              <PrimaryButton
+                className="hours-card__btn"
+                onClick={() => this._showDialog("hoursDialog")}
+                text="Update Hours"
+              />
+              <HoursDialog
+                hidden={hoursDialog}
+                closeDialog={this._closeDialog}
+                currHours={store.hours}
+                onSave={this._onSaveHours}
+              />
+            </div>
           </div>
         </div>
-      </div>
+      </ProfileContext.Provider>
     );
   }
 }
